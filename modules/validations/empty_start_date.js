511f534f9e5b93bde4e64b9d1d986119affc36f2
pedromml <pedrommleao@gmail.com>
2024-03-17 14:27:41 -0300

    Add empty start date validation

---
+++ modules/validations/empty_start_date.js
@@ -0,0 +1,49 @@
+import { t } from '../core/localizer';
+import { utilDisplayLabel } from '../util';
+import { validationIssue, validationIssueFix } from '../core/validation';
+
+export function validationEmptyStartDate() {
+  const type = 'empty_start_date';
+
+  const validation = function checkEmptyStartDate(entity) {
+    // If start_date is not empty, return nothing
+    if (entity.tags && (entity.tags.start_date || entity.tags["start_date:edtf"])) return [];
+    // If entity has no tags, return nothing
+    if (Object.keys(entity.tags).length === 0) return [];
+    // Rule should be ignored for natural entities and waterways
+    if (entity.tags && (entity.tags.natural || entity.tags.waterway || entity.tags.water)) return 0;
+
+    const entityID = entity.id;
+    
+    function showReferenceDate(selection) {
+        selection.selectAll('.issue-reference')
+            .data([0])
+            .enter()
+            .append('div')
+            .attr('class', 'issue-reference')
+            .call(t.append('issues.invalid_format.date.reference'));
+    }
+
+    return [new validationIssue({
+      type: type,
+      severity: 'warning',
+      message: (context) => {
+        const entity = context.hasEntity(entityID);
+        return entity ? t.append('issues.empty_start_date.feature.message', {
+          feature: utilDisplayLabel(entity, context.graph())
+        }) : '';
+      },
+      reference: showReferenceDate,
+      entityIds: [entityID],
+      dynamicFixes: () => {
+        return [
+          new validationIssueFix({ title: t.append('issues.fix.add_start_date.title') })
+        ];
+      }
+    })]
+  };
+
+  validation.type = type;
+  
+  return validation;
+}
--- modules/validations/index.js
+++ modules/validations/index.js
@@ -2,6 +2,7 @@ export { validationAlmostJunction } from './almost_junction';
 export { validationCloseNodes } from './close_nodes';
 export { validationCrossingWays } from './crossing_ways';
 export { validationDisconnectedWay } from './disconnected_way';
+export { validationEmptyStartDate } from './empty_start_date';
 export { validationFormatting } from './invalid_format';
 export { validationHelpRequest } from './help_request';
 export { validationImpossibleOneway } from './impossible_oneway';
